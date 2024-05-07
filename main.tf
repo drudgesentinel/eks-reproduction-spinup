aa2affa9c4e99875a2f7cfe7a51c81b27ddb1940
Sean <sean@seriousbusiness.international>
2024-05-07 13:33:36 -0700

    comment out conflicting admin policies

--- main.tf
+++ main.tf
@@ -113,18 +113,18 @@ resource "aws_eks_addon" "ebs-csi" {
   }
 }
 
-resource "aws_eks_access_entry" "current_user" {
-  cluster_name  = module.eks.cluster_name
-  principal_arn = data.aws_caller_identity.current.arn
+# resource "aws_eks_access_entry" "current_user" {
+#   cluster_name  = module.eks.cluster_name
+#   principal_arn = data.aws_caller_identity.current.arn
 
-}
+# }
 
-resource "aws_eks_access_policy_association" "current_user_admin" {
-  cluster_name  = module.eks.cluster_name
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
-  principal_arn = data.aws_caller_identity.current.arn
+# resource "aws_eks_access_policy_association" "current_user_admin" {
+#   cluster_name  = module.eks.cluster_name
+#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+#   principal_arn = data.aws_caller_identity.current.arn
 
-  access_scope {
-    type = "cluster"
-  }
-}
+#   access_scope {
+#     type = "cluster"
+#   }
+# }
